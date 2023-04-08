89c51889ecca919a6d15b12ff1aa380e93a6c1e4
ArjunRajpal123 <81884929+ArjunRajpal123@users.noreply.github.com>
2023-04-08 15:40:39 -0400

    Updated readme
    1. Added results from both lda packages

--- README.md
+++ README.md
@@ -4,3 +4,23 @@
 # Files and what they have/do
 1. code_1.py - code for using LDA to generate word emeddings
 2. output_Ida.txt - output from LDA + first 10 word embeddings
+
+# Current Results 
+
+
+
+1. LDA - there were two packages that I found that had a pre-built lda code - the one mentioned in our project preposal and another from sklearn - currently our code has only the code from the package mentiond in our project preposal (lda package) and not from sklearn. That being said I tried them both out and produced the following results
+
+LDA package
+Logistic Regresion
+accuracy: 0.5514
+F1 score:  0.6701567600952912
+precision:  0.91144
+recall:  0.5298823310543695
+
+sklearn - lda
+Logistic Regresion
+accuracy: 0.69172
+F1 score:  0.674659124488159
+precision:  0.63928
+recall:  0.714183573152203
--- code_1.py
+++ code_1.py
@@ -11,6 +11,15 @@ from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 nltk.download('stopwords')
+import lda
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_curve, auc
+import matplotlib.pyplot as plt
+
 
 
 root_path = './aclImdb'
@@ -67,14 +76,15 @@ X_val_vec = vectorizer.transform(X_val)
 X_test_vec = vectorizer.transform(test_text)
 
 
-lda = LatentDirichletAllocation(n_components=10, random_state=42)
-lda.fit(X_train_vec)
+lda1 = LatentDirichletAllocation(n_components=10, random_state=42)
+lda2 = lda.LDA(n_topics=10, n_iter=1500, random_state=1)
+lda2.fit(X_train_vec)
 print('finished Latent DirichletAllocation')
 
 
-train_topic_distributions = lda.transform(X_train_vec)
-val_topic_distributions = lda.transform(X_val_vec)
-test_topic_distributions = lda.transform(X_test_vec)
+train_topic_distributions = lda2.fit_transform(X_train_vec)
+val_topic_distributions = lda2.fit_transform(X_val_vec)
+test_topic_distributions = lda2.fit_transform(X_test_vec)
 
 
 lr = LogisticRegression(random_state=42)
@@ -86,6 +96,41 @@ val_preds = lr.predict(val_topic_distributions)
 test_preds = lr.predict(test_topic_distributions)
 print()
 print("Logistic Regresion")
+print("accuracy:", accuracy_score(test_preds,test_labels))
+print("F1 score: ", f1_score(test_preds, test_labels))
+print("precision: ", precision_score(test_preds, test_labels))
+print("recall: ", recall_score(test_preds, test_labels))
+
+# Tokenize text data
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_text)
+
+X_train_seq = tokenizer.texts_to_sequences(train_text)
+X_test_seq = tokenizer.texts_to_sequences(test_text)
+
+vocab_size = len(tokenizer.word_index) + 1
+max_len = max([len(x) for x in X_train_seq])
+X_train_pad = pad_sequences(X_train_seq, maxlen=max_len, padding='post')
+X_test_pad = pad_sequences(X_test_seq, maxlen=max_len, padding='post')
+
+# Define LSTM model architecture
+embedding_dim = 100
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_len))
+model.add(LSTM(units=64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train LSTM model
+model.fit(X_train_pad, train_labels, validation_split=0.2, epochs=5, batch_size=32)
+
+# Evaluate LSTM model performance
+val_preds = model.predict_classes(X_test_pad)
+test_preds = model.predict_classes(X_test_pad)
+
+print()
+print("LSTM")
 print("accuracy:", accuracy_score(test_preds,test_labels))
 print("F1 score: ", f1_score(test_preds, test_labels))
 print("precision: ", precision_score(test_preds, test_labels))
